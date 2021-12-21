9b8b20ca49d3107d3c7ee11aaf0cc00bbc57e41c
Aldhaneka Aufa Izzat <aldhanekaaufaizzat@192.168.1.2>
2021-12-21 18:55:40 +0700

    done

--- index.js
+++ index.js
@@ -1,90 +1,50 @@
-'use strict';
-const path = require('path');
-const {app, BrowserWindow, Menu} = require('electron');
-/// const {autoUpdater} = require('electron-updater');
-const {is} = require('electron-util');
-const unhandled = require('electron-unhandled');
-const debug = require('electron-debug');
-const contextMenu = require('electron-context-menu');
-const config = require('./config.js');
-const menu = require('./menu.js');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const MenuBuilder = require("./menu");
 
-unhandled();
-debug();
-contextMenu();
-
-// Note: Must match `build.appId` in package.json
-app.setAppUserModelId('com.company.AppName');
-
-// Uncomment this before publishing your first version.
-// It's commented out as it throws an error if there are no published versions.
-// if (!is.development) {
-// 	const FOUR_HOURS = 1000 * 60 * 60 * 4;
-// 	setInterval(() => {
-// 		autoUpdater.checkForUpdates();
-// 	}, FOUR_HOURS);
-//
-// 	autoUpdater.checkForUpdates();
-// }
-
-// Prevent window from being garbage collected
-let mainWindow;
-
-const createMainWindow = async () => {
+function createWindow() {
 	const win = new BrowserWindow({
-		title: app.name,
-		show: false,
-		width: 600,
-		height: 400
+		width: "100%",
+		height: "100%",
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+		darkTheme: true,
+		title: "Khanacademy Desktop App",
+		resizable: true,
+		fullscreen: true,
+		fullscreenable: true,
+		minWidth: 1000,
+		minHeight: 800,
+		icon: __dirname + "/static/Icon.png",
 	});
 
-	win.on('ready-to-show', () => {
-		win.show();
-	});
+	let menuBuilder = new MenuBuilder(win);
+	menuBuilder.buildMenu();
 
-	win.on('closed', () => {
-		// Dereference the window
-		// For multiple windows store them in an array
-		mainWindow = undefined;
-	});
-
-	await win.loadFile(path.join(__dirname, 'index.html'));
-
-	return win;
-};
-
-// Prevent multiple instances of the app
-if (!app.requestSingleInstanceLock()) {
-	app.quit();
+	win.loadURL("https://www.khanacademy.org/");
 }
 
-app.on('second-instance', () => {
-	if (mainWindow) {
-		if (mainWindow.isMinimized()) {
-			mainWindow.restore();
-		}
+app.whenReady().then(() => {
+	app.setAboutPanelOptions({
+		applicationName: "KhanAcademy Desktop App",
+		applicationVersion: "1.0",
+		version: "Version",
+		credits: "Credits",
+		copyright: "aldhanekaa",
+		iconPath: __dirname + "/icon.ico",
+	});
+	createWindow();
 
-		mainWindow.show();
-	}
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
 });
 
-app.on('window-all-closed', () => {
-	if (!is.macos) {
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
 		app.quit();
 	}
 });
-
-app.on('activate', async () => {
-	if (!mainWindow) {
-		mainWindow = await createMainWindow();
-	}
-});
-
-(async () => {
-	await app.whenReady();
-	Menu.setApplicationMenu(menu);
-	mainWindow = await createMainWindow();
-
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
-})();
--- menu.js
+++ menu.js
@@ -1,179 +1,156 @@
-'use strict';
-const path = require('path');
-const {app, Menu, shell} = require('electron');
-const {
-	is,
-	appMenu,
-	aboutMenuItem,
-	openUrlMenuItem,
-	openNewGitHubIssue,
-	debugInfo
-} = require('electron-util');
-const config = require('./config.js');
+const { app, Menu } = require('electron');
+module.exports = class MenuBuilder {
+  mainWindow;
 
-const showPreferences = () => {
-	// Show the app's preferences here
+  Languages = {};
+
+  constructor(mainWindow) {
+    this.mainWindow = mainWindow;
+  }
+
+  buildMenu() {
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.DEBUG_PROD === 'true'
+    ) {
+      this.setupDevelopmentEnvironment();
+    }
+
+    const template =
+      process.platform === 'darwin'
+        ? this.buildDarwinTemplate()
+        : this.buildDefaultTemplate();
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    return menu;
+  }
+
+  setupDevelopmentEnvironment() {
+    this.mainWindow.webContents.on('context-menu', (_, props) => {
+      const { x, y } = props;
+
+      Menu.buildFromTemplate([
+        {
+          label: 'Inspect element',
+          click: () => {
+            this.mainWindow.webContents.inspectElement(x, y);
+          },
+        },
+      ]).popup({ window: this.mainWindow });
+    });
+  }
+
+  buildDarwinTemplate() {
+    const subMenuAbout = {
+      label: 'Khan Academy Desktop',
+      submenu: [
+        {
+          label: 'About',
+          selector: 'orderFrontStandardAboutPanel:',
+        },
+
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    };
+    const subMenuEdit = {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:',
+        },
+      ],
+    };
+
+    const subMenuViewProd = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: () => {
+            this.mainWindow.webContents.reload();
+          },
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          },
+        },
+      ],
+    };
+    const subMenuWindow = {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:',
+        },
+        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
+        { type: 'separator' },
+        { label: 'Bring All to Front', selector: 'arrangeInFront:' },
+      ],
+    };
+
+    return [subMenuAbout, subMenuEdit, subMenuViewProd];
+  }
+  buildDefaultTemplate() {
+    const templateDefault = [
+      {
+        label: '&File',
+        submenu: [
+          {
+            label: '&Open',
+            accelerator: 'Ctrl+O',
+          },
+          {
+            label: '&Close',
+            accelerator: 'Ctrl+W',
+            click: () => {
+              this.mainWindow.close();
+            },
+          },
+        ],
+      },
+      {
+        label: '&View',
+        submenu: [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: () => {
+              this.mainWindow.webContents.reload();
+            },
+          },
+          {
+            label: 'Toggle &Full Screen',
+            accelerator: 'F11',
+            click: () => {
+              this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+            },
+          },
+        ],
+      },
+    ];
+
+    return templateDefault;
+  }
 };
-
-const helpSubmenu = [
-	openUrlMenuItem({
-		label: 'Website',
-		url: 'https://github.com/sindresorhus/electron-boilerplate'
-	}),
-	openUrlMenuItem({
-		label: 'Source Code',
-		url: 'https://github.com/sindresorhus/electron-boilerplate'
-	}),
-	{
-		label: 'Report an Issue…',
-		click() {
-			const body = `
-<!-- Please succinctly describe your issue and steps to reproduce it. -->
-
-
----
-
-${debugInfo()}`;
-
-			openNewGitHubIssue({
-				user: 'sindresorhus',
-				repo: 'electron-boilerplate',
-				body
-			});
-		}
-	}
-];
-
-if (!is.macos) {
-	helpSubmenu.push(
-		{
-			type: 'separator'
-		},
-		aboutMenuItem({
-			icon: path.join(__dirname, 'static', 'icon.png'),
-			text: 'Created by Your Name'
-		})
-	);
-}
-
-const debugSubmenu = [
-	{
-		label: 'Show Settings',
-		click() {
-			config.openInEditor();
-		}
-	},
-	{
-		label: 'Show App Data',
-		click() {
-			shell.openItem(app.getPath('userData'));
-		}
-	},
-	{
-		type: 'separator'
-	},
-	{
-		label: 'Delete Settings',
-		click() {
-			config.clear();
-			app.relaunch();
-			app.quit();
-		}
-	},
-	{
-		label: 'Delete App Data',
-		click() {
-			shell.moveItemToTrash(app.getPath('userData'));
-			app.relaunch();
-			app.quit();
-		}
-	}
-];
-
-const macosTemplate = [
-	appMenu([
-		{
-			label: 'Preferences…',
-			accelerator: 'Command+,',
-			click() {
-				showPreferences();
-			}
-		}
-	]),
-	{
-		role: 'fileMenu',
-		submenu: [
-			{
-				label: 'Custom'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'close'
-			}
-		]
-	},
-	{
-		role: 'editMenu'
-	},
-	{
-		role: 'viewMenu'
-	},
-	{
-		role: 'windowMenu'
-	},
-	{
-		role: 'help',
-		submenu: helpSubmenu
-	}
-];
-
-// Linux and Windows
-const otherTemplate = [
-	{
-		role: 'fileMenu',
-		submenu: [
-			{
-				label: 'Custom'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Settings',
-				accelerator: 'Control+,',
-				click() {
-					showPreferences();
-				}
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'quit'
-			}
-		]
-	},
-	{
-		role: 'editMenu'
-	},
-	{
-		role: 'viewMenu'
-	},
-	{
-		role: 'help',
-		submenu: helpSubmenu
-	}
-];
-
-const template = is.macos ? macosTemplate : otherTemplate;
-
-if (is.development) {
-	template.push({
-		label: 'Debug',
-		submenu: debugSubmenu
-	});
-}
-
-module.exports = Menu.buildFromTemplate(template);
